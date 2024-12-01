9cac3547e05418f05cfbc8481b64d6757bd29f0d
HoneyBagerDev <user27384@proton.me>
2024-12-01 20:06:22 +0000

    setup

---
+++ content/contact/index.md
@@ -0,0 +1,10 @@
+---
+layout: single
+title: З'язатись зі мною
+date: 2024-10-02T22:00:00.000Z
+lastmod: 2024-10-03T22:00:00.000Z
+Summary: '-'
+draft: false
+---
+
+На разі зв'язатись зі мною можна тільки через: [Telegram](https://t.me/cat_scan)
---
+++ tina/config.ts
@@ -0,0 +1,76 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "img",
+      publicFolder: "static",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date of creation",
+          },
+          {
+            type: "datetime",
+            name: "lastmod",
+            label: "Date of last review of article",
+          },
+          {
+            type: "string",
+            name: "Summary",
+            label: "What is article about",
+            ui: {
+              component: "textarea",
+            },
+          },
+          {
+            name: "draft",
+            label: "Draft",
+            type: "boolean",
+            required: true,
+            description: "If this is checked the post will not be published",
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
